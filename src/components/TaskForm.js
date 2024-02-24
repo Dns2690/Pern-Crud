ffe728c1b795292561f453c693caf8a033e83878
Dns2690 <dsalazar260990@gmail.com>
2024-02-23 19:26:31 -0600

    Frontent commit

---
+++ src/components/TaskForm.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { Button, Card, CardContent, CircularProgress, Grid, TextField, Typography } from '@mui/material'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function TaskForm() {
+
+  const navigate = useNavigate();
+
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+  })
+
+    const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const res = await fetch(('http://localhost:4000/tasks'), {
+      method: 'POST',
+      body: JSON.stringify(task),
+      headers: {"Content-type": "application/json; charset=UTF-8"}  // Indicates the content ype of the request body.
+    })
+    const data = await res.json();
+    setLoading(false);
+    navigate('/')
+  }
+
+  const handleChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value })
+  }
+
+
+  return (
+    <Grid container direction='column' alignItems='center' justifyContent='center'>
+      <Grid item xs={3}>
+        <Card sx={{ mt: 5 }} style={{
+          backgroundColor: '#1e272e',
+          padding: '1rem'
+        }}>
+          <Typography variant='5' textAlign='center' color='white'>
+            Create Task!
+          </Typography>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+
+              <TextField variant='filled'
+                label='Write your title'
+                sx={{
+                  display: 'block',
+                  margin: '.5rem'
+
+                }}
+                name='title'
+                onChange={handleChange}
+                inputProps={{ style: { color: 'white' } }}
+                InputLabelProps={{ style: { color: "white" } }}
+              />
+              <TextField variant='filled' label='Write your description' multiline rows={4} sx={{
+                display: 'block',
+                margin: '.5rem'
+
+              }}
+                name='description'
+                onChange={handleChange}
+                inputProps={{ style: { color: 'white' } }}
+                InputLabelProps={{ style: { color: "white" } }}
+              />
+
+              <Button variant='contained' color='primary' type='submit'
+              disabled={!task.title || !task.description}>
+                {loading ? <CircularProgress color='inherit' size={24}/>: 'Save'}
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
